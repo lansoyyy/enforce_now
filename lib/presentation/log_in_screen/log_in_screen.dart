bc19c04e2ff4e10089ee6004129d57f59dcbcab9
Lans12345 <olanalans12345@gmail.com>
2023-12-27 12:36:25 +0800

    added auth func

--- lib/presentation/log_in_screen/log_in_screen.dart
+++ lib/presentation/log_in_screen/log_in_screen.dart
@@ -3,7 +3,10 @@ import 'package:enforcenow/widgets/app_bar/appbar_subtitle.dart';
 import 'package:enforcenow/widgets/app_bar/custom_app_bar.dart';
 import 'package:enforcenow/widgets/custom_elevated_button.dart';
 import 'package:enforcenow/widgets/custom_text_form_field.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../../widgets/toast_widget.dart';
 
 // ignore_for_file: must_be_immutable
 class LogInScreen extends StatelessWidget {
@@ -94,7 +97,7 @@ class LogInScreen extends StatelessWidget {
           opacity: 0.7,
           child: Padding(
               padding: EdgeInsets.only(left: 11.h),
-              child: Text("Username*",
+              child: Text("Email*",
                   style: CustomTextStyles.bodyMediumInterBlack90002))),
       SizedBox(height: 1.v),
       Padding(
@@ -124,6 +127,29 @@ class LogInScreen extends StatelessWidget {
 
   /// Navigates to the mainMenuScreen when the action is triggered.
   onTapLogIn(BuildContext context) {
-    Navigator.pushNamed(context, AppRoutes.mainMenuScreen);
+    login(context);
+  }
+
+  login(context) async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: userNameController.text, password: passwordController.text);
+      showToast('Logged in succesfully!');
+      Navigator.pushReplacementNamed(context, AppRoutes.mainMenuScreen);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        showToast("No user found with that email.");
+      } else if (e.code == 'wrong-password') {
+        showToast("Wrong password provided for that user.");
+      } else if (e.code == 'invalid-email') {
+        showToast("Invalid email provided.");
+      } else if (e.code == 'user-disabled') {
+        showToast("User account has been disabled.");
+      } else {
+        showToast("An error occurred: ${e.message}");
+      }
+    } on Exception catch (e) {
+      showToast("An error occurred: $e");
+    }
   }
 }
